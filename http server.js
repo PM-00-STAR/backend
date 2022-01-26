0f147b6b9266186f918b3a5f5cf92388a28fb2e0
PM-00-STAR <pratibhamitra01@gmail.com>
2022-01-26 21:06:32 +0530

    http server making

---
+++ http server.js
@@ -0,0 +1,26 @@
+//Creating http server in node
+const http = require("http")
+const hostname='127.0.0.1'
+const port=3000;
+
+const server = http.createServer((req,res)=>{//callback function
+    res.statusCode= 200;//code of successful response
+    res.setHeader('Content-Type','text/plain');
+    res.end("Welcome to HTTP server");
+})
+server.listen(port,hostname , ()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
+//short me
+const http = require("http")
+const hostname='127.0.0.1'
+const port=3000;
+
+const server = http.createServer((req,res)=>{//callback function
+    res.writeHead(200,{'Content-Type':'text/plain'})
+     res.write("Welcome to HTTP server");
+     res.end()
+}).listen(port,hostname , ()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
+//making http request
