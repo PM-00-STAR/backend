fce290aca8f7c5fd36fdf516792740af059aa731
PM-00-STAR <pratibhamitra01@gmail.com>
2022-01-26 20:37:58 +0530

    async await

--- magic.js
+++ magic.js
@@ -27,7 +27,7 @@ bb()
 
 //using callback( asynchronously)
 function a(msg,callback){
-    setTimeout(()=>{
+    setTimeout(()=>{//callback function uses 2 arguments of success and failure
         console.log(msg)
         callback()
     },1000)
@@ -37,5 +37,43 @@ function b(){
 }
 a("get message",b)
 //asynchronous using promises
+let promise = new Promise(function(resolve,reject){//Promise is a class
+    setTimeout(()=>resolve("Run before"),1000) //callbackfunction
+});
 
+promise.then(
+    result=>{
+        console.log(result)//when promise is resolved
+    GetAfter()
+    },
+    error=>console.log(error)//when promise is rejected
+)
+function GetAfter(){
+    console.log("Print After")
+}
+//GetAfter()
+//async and await are much simpler to make asynchronouus
+function Clow(){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve("hi")
+        },2000)
+    })
+}
 
+// function msg(){//make this function asynchronous
+//     const msg = Clow()
+//     console.log("Message",msg)
+// }
+async function msg(callback){
+    const msg = await Clow()
+    console.log("Message",msg)
+    callback();
+}
+msg(getResult)
+
+function getResult(){
+    console.log("Execute After");
+}
+
+//getResult();
